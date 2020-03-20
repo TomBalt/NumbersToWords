3d40865ba5c4dcaeb27a344fe04d0b2ad7fbb110
TomBalt <baltusis.t@gmail.com>
2020-03-20 19:21:53 +0000

    Pagaliau veikia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,68 +9,56 @@ namespace ConsoleApp1
             string StrNumber = "18467";
             //string StrNumber = Console.ReadLine();
             int num = StrNumber.Length;
-            //  Console.WriteLineLine("{0} {1}", num, num / 3);
             int times = num / 3;
-            //int liekana = 18467 % 1000;
-            //Console.WriteLineLine("Liekana: "+liekana);
             int Number = Convert.ToInt32(StrNumber);
             int dalmuo = 1;
 
-            for (int i = times; i > 0 ; i--)
+            for (int i = times; i >= 0; i--)
             {
-                // Console.WriteLineLine(i+1);
-                // Console.WriteLineLine(Math.Pow(1000, i+1));
-                // Console.WriteLineLine("{0} {1} {2}",i, Math.Pow(1000, i+1), Convert.ToInt16(StrNumber) / Math.Pow(1000,i+1));
-                dalmuo *= 1000;
+                dalmuo = Convert.ToInt32(Math.Pow(1000, i));
                 int liekana = Number % dalmuo;
                 int sveikaDalis = Number / dalmuo;
-                // Console.WriteLineLine($"{i} : {dalmuo} : {liekana} ");
-                
-               
-                if (LengthOfNumber(sveikaDalis) < 3)
-                {
-                   // i += 1;
-                    getShitDone(sveikaDalis,i);
-                }
-                
-                getShitDone(liekana, i-1);
-                Console.ReadKey();
+                getShitDone(sveikaDalis, i);
+                Number -= sveikaDalis * dalmuo;
             }
+            Console.ReadKey();
 
-            static void getShitDone(int number,int i)
+
+        }
+
+        static void getShitDone(int number, int i)
+        {
+            bool NumberFrom10to19 = false;
+            bool NumberFrom20to99 = false;
+            if (LengthOfNumber(number) == 3)
             {
-                bool less19 = false;
-                bool more19 = false;
-                if (LengthOfNumber(number) == 3)
-                {
-                    Console.Write(GetTheRoot(number / 100, less19, more19)  + thousands(i));
-                    //string simtai = "Simtai: " + Convert.ToString(number)[0];
-                }
-                    int tempNR = number % 100;
+                Console.Write(GetTheRoot(number / 100, NumberFrom10to19, NumberFrom20to99) + (number / 100 > 1 ? " simtai" : " simtas"));
+                //string simtai = "Simtai: " + Convert.ToString(number)[0];
+            }
+            int tempNR = number % 100;
 
-                    if (tempNR >= 0 && tempNR <= 9)
-                    {
-                        Console.Write(" " + GetTheRoot(tempNR, less19, more19) + thousands(i));
-                    }
-                    else if (tempNR > 9 && tempNR < 19)
-                    {
-                        less19 = true;
-                        // more19 = false;
-                        Console.Write(" " + GetTheRoot(tempNR % 10, less19, more19) + thousands(i));
-                        //Console.WriteLineLine(GetTheRoot(tempNR % 10, false, true));
-
-                    }
-                    else
-                    {
-                        more19 = true;
-                        Console.Write(GetTheRoot(tempNR / 10, less19, more19) + " " + thousands(i));
-                        more19 = false;
-                        Console.Write(GetTheRoot(tempNR % 10, less19, more19) + " " + thousands(i));
-                    }
-                    //Console.WriteLineLine(GetTheRoot(tempNR, less19, more19));
-                    //Console.WriteLineLine($"Simtai: {simtai} , Desimtys: {less19}");
+            if (tempNR >= 0 && tempNR <= 9)
+            {
+                Console.Write(" " + GetTheRoot(tempNR, NumberFrom10to19, NumberFrom20to99) + thousands(i));
+            }
+            else if (tempNR > 9 && tempNR < 19)
+            {
+                NumberFrom10to19 = true;
+                // NumberFrom20to99 = false;
+                Console.Write(" " + GetTheRoot(tempNR % 10, NumberFrom10to19, NumberFrom20to99) + thousands(i));
+                //Console.WriteLineLine(GetTheRoot(tempNR % 10, false, true));
 
             }
+            else
+            {
+                NumberFrom20to99 = true;
+                Console.Write(" " + GetTheRoot(tempNR / 10, NumberFrom10to19, NumberFrom20to99) + " " + thousands(i));
+                NumberFrom20to99 = false;
+                Console.Write(" " + GetTheRoot(tempNR % 10, NumberFrom10to19, NumberFrom20to99) + " " + thousands(i));
+            }
+            //Console.WriteLineLine(GetTheRoot(tempNR, NumberFrom10to19, NumberFrom20to99));
+            //Console.WriteLineLine($"Simtai: {simtai} , Desimtys: {NumberFrom10to19}");
+
         }
 
         static string thousands(int thousands)
@@ -78,54 +66,54 @@ namespace ConsoleApp1
             switch (thousands)
             {
                 case 0:
-                    return  "";
+                    return "";
                 case 1:
-                    return  " tukstantis ";
+                    return " tukstantis ";
                 case 2:
                     return " milijonas ";
                 default:
-                    return  " Not valid ";
+                    return " Not valid ";
             }
-            
+
         }
-        static string GetTheRoot(int number, bool less19, bool more19)
+        static string GetTheRoot(int number, bool NumberFrom10to19, bool NumberFrom20to99)
         {
             string result;
             switch (number)
             {
                 case 0:
-                    result = (less19 ? "Desimt" : "Nulis");
+                    result = (NumberFrom10to19 ? "desimt" : "Nulis");
                     return result;
                 case 1:
-                    result = "Vien" + (less19 ? "uolika" : "as");
+                    result = "vien" + (NumberFrom10to19 ? "uolika" : "as");
                     return result;
                 case 2:
-                    result = "D" + (less19 ? "vylika" : (more19 ? "dvidesimt" : "u"));
+                    result = "d" + (NumberFrom10to19 ? "vylika" : (NumberFrom20to99 ? "dvidesimt" : "u"));
                     return result;
                 case 3:
-                    result = "Try" + (less19 ? "lika" : (more19 ? "trisdesimt" : "s"));
+                    result = "try" + (NumberFrom10to19 ? "lika" : (NumberFrom20to99 ? "trisdesimt" : "s"));
                     return result;
 
                 case 4:
-                    result = "Keturi" + (less19 ? "olika" : (more19 ? "keturiasdesimt" : ""));
+                    result = "keturi" + (NumberFrom10to19 ? "olika" : (NumberFrom20to99 ? "keturiasdesimt" : ""));
                     return result;
 
                 case 5:
-                    result = "Penki" + (less19 ? "olika" : (more19 ? "penkiasdesimt" : ""));
+                    result = "penki" + (NumberFrom10to19 ? "olika" : (NumberFrom20to99 ? "penkiasdesimt" : ""));
                     return result;
 
                 case 6:
-                    result = "Sesi" + (less19 ? "olika" : (more19 ? "sesiasdesimt" : ""));
+                    result = "sesi" + (NumberFrom10to19 ? "olika" : (NumberFrom20to99 ? "sesiasdesimt" : ""));
                     return result;
 
                 case 7:
-                    result = "Sept" + (less19 ? "iniolika" : (more19 ? "septyniasdesimt" : "yni"));
+                    result = "sept" + (NumberFrom10to19 ? "iniolika" : (NumberFrom20to99 ? "septyniasdesimt" : "yni"));
                     return result;
                 case 8:
-                    result = "Astuoni" + (less19 ? "olika" : (more19 ? "astuonesdiasimt" : ""));
+                    result = "astuoni" + (NumberFrom10to19 ? "olika" : (NumberFrom20to99 ? "astuonesdiasimt" : ""));
                     return result;
                 case 9:
-                    result = "Devyni" + (less19 ? "olika" : (more19 ? "devyniasdesimt" : ""));
+                    result = "devyni" + (NumberFrom10to19 ? "olika" : (NumberFrom20to99 ? "devyniasdesimt" : ""));
                     return result;
                 default:
                     return "Nothing";
