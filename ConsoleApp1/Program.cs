d3f6a715b4421ccea023e66a3be8292a311d511d
TomBalt <baltusis.t@gmail.com>
2020-03-21 19:28:40 +0000

    Added function ChangeOnesToText.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,30 +7,31 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-
-            
             Console.Write("Please enter a number: ");
-            string Input = Console.ReadLine();
+            string InputString = Console.ReadLine();
             Console.WriteLine("Task: Is it a number?");
-            Console.WriteLine($"Input {Input} {(IsItANumber(Input) ? "is" : "is not")} a number.");
+            Console.WriteLine($"Input {InputString} {(IsItANumber(InputString) ? "is" : "is not")} a number.");
             Console.WriteLine("====================================================================");
+
             int RangeMin = -9;
             int RangeMax = 9;
             bool IsItAValidNumber;
             //*
             Console.WriteLine($"Task: Number is between {RangeMin} and {RangeMax}?");
             Console.Write($"Please enter a number between {RangeMin} and {RangeMax}: ");
-            Input = Console.ReadLine();
-            if (IsItANumber(Input))
+            InputString = Console.ReadLine();
+            int InputInt;
+            if (IsItANumber(InputString))
             {
-                IsItAValidNumber = IsANumberInArange(Input, RangeMin, RangeMax);
+                InputInt = Convert.ToInt32(InputString);
+                IsItAValidNumber = IsANumberInArange(InputInt, RangeMin, RangeMax);
             }
             Console.WriteLine("====================================================================");
-            ////
+
             Console.WriteLine("Task: Change number in to the text.");
             Console.Write("Please enter a number to be converted to sentence: ");
-            Input = Console.ReadLine();
-            ChangeNumberToText(Convert.ToInt32(Input));
+            InputInt = Convert.ToInt32(Console.ReadLine());
+            ChangeNumberToText(InputInt);
             Console.WriteLine("====================================================================");
             //*/
 
@@ -50,12 +51,12 @@ namespace ConsoleApp1
             RangeMin = -19;
             RangeMax = 19;
             Console.WriteLine($"Task: Number is between {RangeMin} and {RangeMax}?");
-            Input = Console.ReadLine();
+            InputInt = Convert.ToInt32(Console.ReadLine());
 
-            IsItAValidNumber = IsANumberInArange(Input, RangeMin, RangeMax);
+            IsItAValidNumber = IsANumberInArange(InputInt, RangeMin, RangeMax);
             if (IsItAValidNumber)
             {
-                ChangeNumberToText(Convert.ToInt32(Input));
+                ChangeNumberToText(Convert.ToInt32(InputInt));
             }
             else
             {
@@ -64,19 +65,59 @@ namespace ConsoleApp1
             //*/
             Console.WriteLine("====================================================================");
 
+            Console.WriteLine("Task: Change number in to the text - ChangeOnesToText.");
+            Console.Write("Please enter a number between -999 to 999 to be converted to sentence: ");
+            string words = "";
+            RangeMin = -999;
+            RangeMax = 999;
+            int InputNumber = Convert.ToInt32(Console.ReadLine());
+            if (IsANumberInArange(InputNumber, RangeMin, RangeMax))
+            {
+                ChangeOnesToText(InputNumber, ref words);
+                Console.WriteLine(FirstLetterUppercase(words));
+            }
+            Console.WriteLine("====================================================================");
         }
 
-        static bool IsANumberInArange(string StrNumber, int RangeMin = -9, int RangeMax = 9)
+        static void ChangeOnesToText(Int32 Number, ref string words)
         {
-            int IntNumber = Convert.ToInt32(StrNumber);
+
+            if (Number < 0)
+            {
+                words += "minus ";
+                Number *= -1;
+            }
+            int NumberOfThousands = Convert.ToString(Number).Length / 3;
+            for (int i = NumberOfThousands; i >= 0; i--)
+            {
+                int divisor = Convert.ToInt32(Math.Pow(1000, i));
+                int integer = Number / divisor;
+                if (integer == 0 && Number != 0)
+                {
+                    continue;
+                }
+                ConvertNumberToTextV2(integer, i, ref words);
+                Number -= integer * divisor;
+                if (Number == 0)
+                {
+                    break;
+
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static bool IsANumberInArange(int IntNumber, int RangeMin = -9, int RangeMax = 9)
+        {
             bool result = (IntNumber > RangeMin && IntNumber < RangeMax) ? true : false;
             if (result)
             {
-                Console.WriteLine($"Number {StrNumber} {(result ? "is" : " is not")} in a range between {RangeMin} and {RangeMax}.");
+                Console.WriteLine($"Number {IntNumber} {(result ? "is" : " is not")} in a range between {RangeMin} and {RangeMax}.");
             }
             else
             {
-                Console.WriteLine($"Input {StrNumber} is not a valid number.");
+                Console.WriteLine($"Input {IntNumber} is not a valid number.");
             }
             return result;
         }
@@ -196,40 +237,40 @@ namespace ConsoleApp1
                     words.Add(GetTheRoot(tempNR % 10, NumberFrom10to19, NumberFrom20to99));
                 }
             }
-            words.Add(thousands(thousandIndex, tempNR));
+            words.Add(Thousands(thousandIndex, tempNR));
         }
-        static string ExtractThousandsEnding(int number, int thousandsIndex)
+        static string ExtractThousandsEnding(int number, int ThousandsIndex)
         {
             string ending;
             if (number == 1)
             {
-                ending = thousandsIndex == 1 ? "tis" : "as";
+                ending = ThousandsIndex == 1 ? "tis" : "as";
             }
             else if (number > 1 && number <= 9)
             {
-                ending = thousandsIndex == 1 ? "ciai" : "ai";
+                ending = ThousandsIndex == 1 ? "ciai" : "ai";
             }
             else if (number > 9 && number <= 20 || number % 10 == 0)
             {
-                ending = thousandsIndex == 1 ? "ciu" : "u";
+                ending = ThousandsIndex == 1 ? "ciu" : "u";
             }
             else
             {
-                ending = ExtractThousandsEnding(number % 10, thousandsIndex);
+                ending = ExtractThousandsEnding(number % 10, ThousandsIndex);
             }
             return ending;
         }
 
-        static string thousands(int thousands, int tempNR)
+        static string Thousands(int Thousands, int tempNR)
         {
-            switch (thousands)
+            switch (Thousands)
             {
                 case 0:
                     return "";
                 case 1:
-                    return "tukstan" + ExtractThousandsEnding(tempNR, thousands);
+                    return "tukstan" + ExtractThousandsEnding(tempNR, Thousands);
                 case 2:
-                    return "milijon" + ExtractThousandsEnding(tempNR,thousands);
+                    return "milijon" + ExtractThousandsEnding(tempNR, Thousands);
                 default:
                     return "Over the limit!";
             }
@@ -290,5 +331,44 @@ namespace ConsoleApp1
             a[0] = char.ToUpper(a[0]);
             return new string(a);
         }
+
+        static void ConvertNumberToTextV2(int number, int thousandIndex, ref string words)
+        {
+            bool NumberFrom10to19 = false;
+            bool NumberFrom20to99 = false;
+            if (LengthOfNumber(number) == 3)
+            {
+                words += GetTheRoot(number / 100, NumberFrom10to19, NumberFrom20to99) + (number / 100 > 1 ? " simtai" : " simtas");
+                words += " ";
+            }
+            int tempNR = number % 100;
+
+            if (tempNR >= 0 && tempNR <= 9)
+            {
+                words += GetTheRoot(tempNR, NumberFrom10to19, NumberFrom20to99);
+                words += " ";
+            }
+            else if (tempNR > 9 && tempNR <= 19)
+            {
+                NumberFrom10to19 = true;
+                words += GetTheRoot(tempNR % 10, NumberFrom10to19, NumberFrom20to99);
+                words += " ";
+            }
+            else
+            {
+                NumberFrom20to99 = true;
+                words += GetTheRoot(tempNR / 10, NumberFrom10to19, NumberFrom20to99);
+                words += " ";
+                NumberFrom20to99 = false;
+                if (tempNR % 10 != 0)
+                {
+                    words += GetTheRoot(tempNR % 10, NumberFrom10to19, NumberFrom20to99);
+                    words += " ";
+                }
+            }
+            words += Thousands(thousandIndex, tempNR);
+            words += " ";
+        }
     }
+
 }
